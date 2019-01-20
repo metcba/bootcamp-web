78d80386d3a3ce71fb68c84bb325a7739f46c98c
Milagro Teruel <milagro.teruel@gmail.com>
2019-01-20 09:28:25 -0300

    Applying new style, fixing nav and footer

--- src/app/mentors/mentors.component.ts
+++ src/app/mentors/mentors.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-mentors',
   templateUrl: './mentors.component.html',
@@ -13,20 +14,22 @@ export class MentorsComponent implements OnInit {
 
   ngOnInit() {
     this.speakers = [
-      {name: 'Pipi Márquez', photo: 'assets/karen.jpg',
-       desc: 'Who are in extremely love with eco friendly system.'},
-      {name: 'Vicky Zárate', photo: 'assets/karen.jpg',
-       desc: 'Who are in extremely love with eco friendly system.'},
+      {name: 'Pipi Márquez', photo: 'assets/pipi.jpg',
+       desc: 'Diseñadora UX en Naranja. Cofundadora en Moles'},
+      {name: 'Vicky Zárate', photo: 'assets/vicky.jpg',
+       desc: 'Diseñadora UX en Naranja. Cofundadora en Moles'},
+      {name: 'Mili Teruel', photo: 'assets/mili.png',
+       desc: 'Doctorando en Machine Learning. Cofundadora en MeTCba.'},
     ]
     this.mentors = [
       {name: 'Dani Bosch', photo: 'assets/dani2.jpg',
-       desc: 'Who are in extremely love with eco friendly system.'},
+       desc: 'UX Developer Jr. en Cintelink'},
       {name: 'Karen Haag', photo: 'assets/karen.jpg',
-       desc: 'Who are in extremely love with eco friendly system.'},
-      {name: 'Male Morales', photo: 'assets/karen.jpg',
-       desc: 'Who are in extremely love with eco friendly system.'},
-      {name: 'Mili Teruel', photo: 'assets/karen.jpg',
-       desc: 'Who are in extremely love with eco friendly system.'},
+       desc: 'Software Engineer. Data Scientist en BITLOGIC.io'},
+      {name: 'Male Morales', photo: 'assets/male.jpg',
+       desc: '(Casi) Licenciada en Computación. Amante de la lógica y de los métodos formales. Futura doctorando :)'},
+      {name: 'Laura Alonso', photo: 'assets/lau.jpg',
+       desc: 'Doctora en Lingüística Computacional. Profesora en la UNC.'},
     ]
   }
 
