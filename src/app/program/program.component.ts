697933a703aac9bbc9d4aff86ada556b72bbb760
Milagro Teruel <milagro.teruel@gmail.com>
2019-01-21 00:51:26 -0300

    Fixing all links

--- src/app/program/program.component.ts
+++ src/app/program/program.component.ts
@@ -12,10 +12,10 @@ export class ProgramComponent implements OnInit {
 
   ngOnInit() {
     this.program = [
-      {day: "Actividad previa", desc: "Te proponemos que ...."},
-      {day: "Lunes 18", desc: "Intro a diseño y experiencia de usuario. Actividad de diseño de solución colaborativa."},
-      {day: "Martes 19", desc: "Programación del front-end. Maquetación con html y frameworks css. Intro a JavaScript."},
-      {day: "Miércoles 20", desc: "Frameworks web: Angujar. Organización del'proyecto con componentes y módulos."},
+      {day: "Actividad previa", desc: "Te enviaremos material complementario para que te familiarices con nociones de programación y puedas configurar el entorno de trabajo."},
+      {day: "Lunes 18", desc: "Intro a diseño y experiencia de usuario. Actividad de diseño de solución colaborativa sobre casos reales."},
+      {day: "Martes 19", desc: "Programación del front-end. Evaluación de plantillas y maquetación en pocos pasos con html y frameworks css. Intro a JavaScript."},
+      {day: "Miércoles 20", desc: "Frameworks web: Angujar5. Organización del proyecto con componentes y módulos."},
       {day: "Jueves 21", desc: "Programación de un backend básico con node.js. Hostings y dominios. GitHub Pages"},
       {day: "Viernes 22", desc: "¡Puesta en común con la organización y despliegue final!"},
     ]
