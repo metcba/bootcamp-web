dd330face9faf4aeecbea648f87a8048eaf29ff8
Milagro Teruel <milagro.teruel@gmail.com>
2019-01-20 21:29:41 -0300

    Adding bootcamp page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
