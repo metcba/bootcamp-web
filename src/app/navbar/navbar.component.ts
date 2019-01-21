73dfd92083729202b7ed971dc32b47ea12f94b1f
Milagro Teruel <milagro.teruel@gmail.com>
2019-01-20 23:24:32 -0300

    Removing fixed navbar and adding icon

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -15,43 +15,31 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 )]
 })
 export class NavbarComponent implements OnInit {
-  private _fixed = '';
+  navbarOpen = false;
 
   ngOnInit() { }
-
-  @Input()
-  set fixed(fixed: string) {
-    console.log(fixed);
-    this._fixed = (fixed == 'fixed');
-    console.log(this._fixed);
-  }
-
-  get fixed(): string { return this._fixed; }
-
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e) {
-    if (!this._fixed) {
-      let navbar = document.getElementById('bc-navbar');
-      let offset = window.pageYOffset;
+    let navbar = document.getElementById('bc-navbar');
+    let offset = window.pageYOffset;
 
-      if (offset > 150 && !navbar.classList.contains('scrolled')) {
-        navbar.classList.add('scrolled');
-      }
+    if (offset > 150 && !navbar.classList.contains('scrolled')) {
+      navbar.classList.add('scrolled');
+    }
 
-      if (offset < 150 && navbar.classList.contains('scrolled')) {
-        navbar.classList.remove('scrolled', 'sleep');
-      }
-      if (offset > 350 && !navbar.classList.contains('awake')) {
-        navbar.classList.add('awake');
-      }
-      if (offset < 350 && navbar.classList.contains('awake')) {
-        navbar.classList.remove('awake');
-        navbar.classList.add('sleep');
-      }
+    if (offset < 150 && navbar.classList.contains('scrolled')) {
+      navbar.classList.remove('scrolled', 'sleep');
+    }
+    if (offset > 350 && !navbar.classList.contains('awake')) {
+      navbar.classList.add('awake');
+    }
+    if (offset < 350 && navbar.classList.contains('awake')) {
+      navbar.classList.remove('awake');
+      navbar.classList.add('sleep');
     }
   }
 }
