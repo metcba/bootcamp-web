b3023b7a44a63a46de5a941ec1ba76968ac9a3f2
Milagro Teruel <milagro.teruel@gmail.com>
2019-02-20 10:32:16 -0300

    latests sponsors

--- src/app/mentors/mentors.component.ts
+++ src/app/mentors/mentors.component.ts
@@ -20,16 +20,20 @@ export class MentorsComponent implements OnInit {
        desc: 'Diseñadora UX en Naranja. Cofundadora en Moles'},
       {name: 'Mili Teruel', photo: 'assets/mili.png',
        desc: 'Doctorando en Machine Learning. Cofundadora en MeTCba.'},
-      {name: 'Dani Bosch', photo: 'assets/dani2.jpg',
-       desc: 'UX Developer Jr. en Cintelink'},
+      {name: 'Eugenia Revol', photo: 'assets/euge.jpg',
+       desc: 'Web UI Developer Semisenior en Globant'},
     ]
     this.mentors = [
+      {name: 'Dani Bosch', photo: 'assets/dani2.jpg',
+       desc: 'UX Developer Jr. en Cintelink'},
       {name: 'Karen Haag', photo: 'assets/karen.jpg',
        desc: 'Software Engineer. Data Scientist en BITLOGIC.io'},
       {name: 'Male Morales', photo: 'assets/male.jpg',
        desc: '(Casi) Licenciada en Computación. Amante de la lógica y de los métodos formales. Futura doctorando.'},
       {name: 'Antonella Sgarlatta', photo: 'assets/anto.jpg',
        desc: 'Ingeniera Biomédica. Software Developer. Data Scientist'},
+      {name: 'Virginia Dal Lago', photo: 'assets/vir.jpg',
+       desc: 'Machine Learning Developer en Mercado Libre. Doctora en Física y Licenciada en  Química.'},
       {name: 'Laura Alonso', photo: 'assets/lau.jpg',
        desc: 'Doctora en Lingüística Computacional. Profesora en la UNC.'},
     ]
--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -12,16 +12,15 @@ export class SponsorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.organizers = [
+    this.sponsors = [
       {name: 'MeTCba', img: 'assets/metcba.png', site: 'https://metcba.org'},
       {name: 'Globant', img: 'assets/globant.jpg', site: 'https://www.globant.com'},
       {name: 'BitLogic', img: 'assets/bitlogic.png', site: 'https://bitlogic.io/'},
-    ]
-    this.sponsors = [
       {name: 'McAfee', img: 'assets/mcafee.png', site: 'https://home.mcafee.com/'},
       {name: 'Cintelink', img: 'assets/cintelink.png', site: 'https://cintelink.com.ar/'},
       {name: 'BitLogic', img: 'assets/binamp.png', site: 'http://binamp.com/'},
-      //{name: 'Women Tech Makers', img: 'assets/wtm.png', site: 'https://www.womentechmakers.com/'},
+      {name: 'Mercado Libre', img: 'assets/mercadolibre.png', site: 'https://developers.mercadolibre.com.ar'},
+      {name: 'ESolutions', img: 'assets/esolutions2.png', site: 'http://www.esolutions.com.ar/web/'},
     ]
   }
 
