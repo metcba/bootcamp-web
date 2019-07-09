aea6598f8fe06f47e7b0bd44a0764c720b0ea348
Daniela Bosch <danielarbosch@gmail.com>
2019-07-09 13:00:43 -0300

    Mentoras

--- src/app/app-jul2019/mentors/mentors.component.ts
+++ src/app/app-jul2019/mentors/mentors.component.ts
@@ -6,26 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mentors.component.scss']
 })
 export class MentorsComponent implements OnInit {
-  speakers: any;
-  mentors: any;
+  participants: any;
 
   constructor() { }
 
   ngOnInit() {
-    this.speakers = [
+    this.participants = [
         {
-            name: 'Nombre Apellido', 
-            photo: 'assets/foto.jpg',
-            description: 'UX Developer @ Empresa'
+            name: 'Adri Calero',
+            photo: 'adri.jpg',
+            description: 'UX Lead @ Naranja',
+            type: 'Speaker',
+            linkedin: 'https://www.linkedin.com/in/adricalero'
         },
-    ]
-    this.mentors = [
         {
-            name: 'Nombre Apellido', 
-            photo: 'assets/foto.jpg',
-            description: 'UX Developer @ Empresa'
+            name: 'Agus Demaldé',
+            photo: 'agus.jpg',
+            type: 'Speaker y Mentora',
+            description: 'Ingeniera en Sistemas de Información. Web UI Developer @ Mercado Libre',
+            linkedin: 'https://www.linkedin.com/in/agustina-demald%C3%A9-6969ba114/'
         },
-        
-    ]
+        {
+            name: 'Dani Bosch',
+            photo: 'dani.jpg',
+            type: 'Mentora',
+            description: 'Estudiante de la Lic. en Cs. de la Computación. UX Developer @ Cintelink',
+            linkedin: 'https://www.linkedin.com/in/danielabosch/'
+        },
+        {
+            name: 'Naho Puente',
+            photo: 'naho.jpeg',
+            type: 'Mentora',
+            description: 'Estudiante de la Lic. en Cs. de la Computación. Developer @ Bitlogic',
+            linkedin: 'https://www.linkedin.com/in/nahomy-puentec/'
+        },
+        {
+            name: 'Juli Pereyra Sarry',
+            photo: 'juli.jpg',
+            type: 'Speaker',
+            description: 'UX designer @ Mercado Libre',
+            linkedin: 'https://www.linkedin.com/in/julia-pereyra-sarry-010384131/'
+        },
+    ];
+
+    this.shuffle(this.participants);
+  }
+
+  shuffle(array: Array<any>): void {
+    array.sort(() => Math.random()-0.5);
   }
 }
